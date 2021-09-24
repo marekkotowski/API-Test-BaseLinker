55876c3a45b4a66772d2f3f49d1ae9404e0afb9d
marekkotowski <marek-kotowski@o2.pl>
2021-09-24 07:09:37 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_Test_BaseLinker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Modele.APIConnectModel APIConnect = new Modele.APIConnectModel();
+
+            try
+            {
+                APIConnect.PobierzZamówieniaOdDnia(DateTime.Now);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+    }
+}
